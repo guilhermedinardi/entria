4674f06fe51887147d5c0b13d27c981abccaa33c
Guilherme Dinardi <gdinardito@gmail.com>
2021-07-08 00:38:41 -0300

    fix startServer

--- server/index.js
+++ server/index.js
@@ -1,5 +1,2 @@
-import startServer from '../server/server'
-import typeDefs from '../server/graphql/typeDefs'
-import resolvers from '../server/graphql/resolvers'
-
-startServer({ typeDefs, resolvers })
+import startServer from './server'
+startServer()
