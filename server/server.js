06d574c05a8fcf2033be9d943c79b3a9c1fc2230
Guilherme Dinardi <gdinardito@gmail.com>
2021-07-08 00:39:26 -0300

    fix server.js

--- server/server.js
+++ server/server.js
@@ -1,15 +1,15 @@
 import { ApolloServer } from 'apollo-server';
 import mongoose from 'mongoose';
+import { schema } from './src/schema/schema';
 
-
-function startServer({ typeDefs, resolvers }) {
+function startServer() {
     mongoose.connect('mongodb://localhost:27017/graphql', {
         useNewUrlParser: true,
         useUnifiedTopology: true,
     })
 
-    const server = new ApolloServer({ typeDefs, resolvers })
-    server.listen().then(({ url }) => console.log(`server started at ${url}`))
+    const server = new ApolloServer({ schema })
+    server.listen().then(({ url }) => console.log(`🔥 server started at ${url}`))
 }
 
 export default startServer;
