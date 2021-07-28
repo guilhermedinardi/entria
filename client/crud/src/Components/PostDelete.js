9f66d5efbb3e70d58983307dc6d4e27f97943a9a
Guilherme Dinardi <gdinardito@gmail.com>
2021-07-27 22:33:51 -0300

    fix updater on component PostDeleteMutation

--- client/crud/src/Components/PostDelete.js
+++ client/crud/src/Components/PostDelete.js
@@ -1,4 +1,8 @@
+import { useCallback } from 'react';
 import graphql from 'babel-plugin-relay/macro';
+import { ConnectionHandler } from 'relay-runtime';
+import { useMutation } from "react-relay";
+
 
 export const PostDeleteMutation = graphql `
   mutation PostDeleteMutation($input: PostDeleteInput!){
@@ -9,12 +13,61 @@ export const PostDeleteMutation = graphql `
     },
   }
 `;
-export const postDeleteOptimisticResponse = post => ({
-  PostDeleteMutation: {
-    success: '',
-    error: null,
-    post: {
-      id: post.id,
-    },
-  },
-});
+function sharedUpdater(
+  store,
+  deleteId
+) {
+  const connection = ConnectionHandler.getConnection(
+     store.getRoot(), 
+    "PostHome_posts"
+  );
+
+  if (!connection) {
+    return;
+  }
+  ConnectionHandler.deleteNode(connection, deleteId);
+}
+
+export default function useRemovePostMutation() {
+  const [commit] = useMutation(PostDeleteMutation);
+  return [
+    useCallback(
+      (postId) => {
+        const input = {
+          postId
+        };
+
+        return commit({
+          variables: {
+            input
+          },
+          updater: (store) => {
+
+            const payload = store.getRootField("PostDeleteMutation");
+
+            if (!payload) {
+              return;
+            }
+
+            const deletedTodoId = payload.getValue("deleteId");
+            
+
+            if (typeof deletedTodoId !== "string") {
+              throw new Error(
+                `Expected removeTodo.deletedTodoId to be string, but got: ${typeof deletedTodoId}`
+              );
+            }
+
+            sharedUpdater(store, postId );
+          },
+
+          optimisticUpdater: (store) => {
+            sharedUpdater(store, postId);
+          }
+
+        });
+      },
+      [commit]
+    )
+  ]; 
+}
