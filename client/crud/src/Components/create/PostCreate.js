302a1c148a3c9616ff0723c916eddc59480878fa
Guilherme Dinardi <gdinardito@gmail.com>
2021-08-02 21:28:51 -0300

    fix added PostCreate component

--- client/crud/src/Components/create/PostCreate.js
+++ client/crud/src/Components/create/PostCreate.js
@@ -8,64 +8,71 @@ import { default as usePostCreateMutation } from './PostCreateMutation'
 const PostCreate = (props) => {
     const post = useFragment(
         graphql` 
-            fragment PostCreate on Post {
+            fragment PostCreate_post on Post {
                 title
                 content
                 tag
                 link
                 id
-            }
+            },
         `,
+        
         props.post,
     ); 
 
-const [title, setTitle] = useState('')
-const [content, setContent] = useState('')
-const [tag, setTag] = useState('')
-const [link, setLink] = useState('') 
+    const [title, setTitle] = useState('')
+    const [content, setContent] = useState('')
+    const [tag, setTag] = useState('')
+    const [link, setLink] = useState('') 
 
-const [addTool] = usePostCreateMutation()
+    const [addTool] = usePostCreateMutation()
 
-const handlePostCreate = useCallback(
-    (PostCreateInput) => {
-      addTool(PostCreateInput, post.id);
-    },
-    [addTool, post.id]
-  )
-return (
-    <PostCreate>
-        <input 
-        value={title}
-        onChange={(e) => setTitle(e.target.value)}
-        title={"Name"}
-        type={"text"}
-        placeholder={"Enter tool"}
-        />
-        <input 
-        value={content}
-        onChange={(e) => setContent(e.target.value)}
-        title={"Content"}
-        type={"text"}
-        placeholder={"Enter content tool"}
-        />
-        <input 
-        value={tag}
-        onChange={(e) => setTag(e.target.value)}
-        title={"tag"}
-        type={"text"}
-        placeholder={"Enter tag tool"}
-        />
-        <input 
-        value={link}
-        onChange={(e) => setLink(e.target.value)}
-        title={"Link"}
-        type={"text"}
-        placeholder={"Enter link tool"}
-        />
-        <button onClick={handlePostCreate(console.log('aqui adicionou'))}>
-            +AddTool
-        </button>
-    </PostCreate>
+    const handlePostCreate = (event) => {
+            
+            event.preventDefault()
+            const payload = { 
+                title,
+                content,
+                tag,
+                link,
+              }
+              
+              addTool(payload)
+            }
+    return (
+        <form onSubmit={handlePostCreate}>
+            <input 
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            title={"title"}
+            type={"text"}
+            placeholder={"Enter tool"}
+            />
+            <input 
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
+            title={"Content"}
+            type={"text"}
+            placeholder={"Enter content tool"}
+            />
+            <input 
+            value={tag}
+            onChange={(e) => setTag(e.target.value)}
+            title={"tag"}
+            type={"text"}
+            placeholder={"Enter tag tool"}
+            />
+            <input 
+            value={link}
+            onChange={(e) => setLink(e.target.value)}
+            title={"Link"}
+            type={"text"}
+            placeholder={"Enter link tool"}
+            />
+            <button type='submit'>
+                +AddTool
+            </button>
+        </form>
     )
 }
 export default PostCreate
