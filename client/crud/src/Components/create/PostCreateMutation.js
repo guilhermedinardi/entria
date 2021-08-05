2f95b5d7c646801b0f862d6be462624446e566e5
Guilherme Dinardi <gdinardito@gmail.com>
2021-08-04 23:36:35 -0300

    fix console.log and indentation

--- client/crud/src/Components/create/PostCreateMutation.js
+++ client/crud/src/Components/create/PostCreateMutation.js
@@ -17,22 +17,20 @@ const mutation = graphql`
     }
   }
 `
-function sharedUpdater(
+ function sharedUpdater(
   store,
-  postCreate,
-  newEdge
-) {
-  console.log(store, store.getRoot())
+  edge
+) { 
   const connection = ConnectionHandler.getConnection(
-     store.getRoot(), 
+    store.getRoot(), 
     "PostHome_posts"
   );
-  console.log({connection})
+
   if (!connection) {
     return;
   }
-  ConnectionHandler.insertEdgeAfter(connection, newEdge);
-}
+  ConnectionHandler.insertEdgeAfter(connection, edge);
+} 
 
 let tempID = 0;
 
@@ -40,52 +38,49 @@ export default function usePostCreateMutation() {
   const [commit] = useMutation(mutation);
   return [
     useCallback(
-      (PostCreateInput) => {
-        const input = {
-          PostCreateInput
-        };
+      (postCreateInput) => {
+        const input = postCreateInput
 
         return commit({
           variables: {
             input
-          },
+          }, 
           updater: (store) => {
 
-            const payload = store.getRootField("PostCreateMutation");
+          const payload = store.getRootField("PostCreateMutation");
 
-            if (!payload) {
-              return;
-            }
+          if (!payload) {
+            return;
+          }
 
-            const newEdge = payload.getLinkedRecord("postEdge");
-            
+          const newEdge = payload.getLinkedRecord("postEdge");
+          
+          if (!newEdge) {
+            return;
+          }
 
-            if (!newEdge) {
-              return;
-            }
-
-            sharedUpdater(store, newEdge);
-          },
-
+          sharedUpdater(store, newEdge);
+          }, 
+ 
           optimisticUpdater: (store) => {
-            const id = "client:newTodo:" + tempID++;
+            const id = "client:newEdge:" + tempID++;
 
-            const node = store.create(id, "Post");
+            const node = store.create(input, "Post");
 
-            node.setValue(id, "id");
+            node.setValue(id, "id"); 
 
-            node.setValue(PostCreateInput, "posts");
+            node.setValue(postCreateInput, "posts"); 
 
-            const newEdge = store.create(
+            const newPost = store.create(
               "client:newEdge:" + tempID++,
               "postEdge"
             );
-            newEdge.setLinkedRecord(node, "node");
-            sharedUpdater(store, newEdge);
-          },
-        })
+            newPost.setLinkedRecord(node, "node");
+            sharedUpdater(store, newPost); 
+          }, 
+        }) 
       },
       [commit]
-    ),
+      ),
   ]
 }
