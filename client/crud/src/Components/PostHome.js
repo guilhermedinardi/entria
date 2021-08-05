a9917c41761f0c1ccb20689d9bb8ead763b118e0
Guilherme Dinardi <gdinardito@gmail.com>
2021-08-04 23:34:42 -0300

    added style on page

--- client/crud/src/Components/PostHome.js
+++ client/crud/src/Components/PostHome.js
@@ -1,9 +1,37 @@
 import React, { useCallback } from 'react'
 import graphql from 'babel-plugin-relay/macro';
 import { usePaginationFragment } from 'react-relay';
+import styled from 'styled-components'
 
 import Post from './Post'
-import PostCreate from './create/PostCreate'
+
+const Home = styled.div`
+  .home{
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+    gap: 20px;
+    width: 100%;
+  }
+  .content-btn{
+    display: flex;
+    justify-content: center;
+    .btn-load-posts{
+      border: none;
+      border-radius: 20px;
+      margin: 2em;
+      width: 20%;
+      height: 5vh;
+      background-color: #5173E3;
+      color: #ffffff;
+      cursor: pointer;
+      font-size: 0.8em;
+        &:hover{
+          background: #ffffff;
+          color: #5173E3;
+        }
+    }
+  }
+`
 
 const PostHome = (props) => {
   const { data, loadNext, isLoadingNext } = usePaginationFragment(
@@ -40,15 +68,16 @@ const PostHome = (props) => {
   }, [isLoadingNext, loadNext]);
 
   return (
-    <>
-    <div>
-      <PostCreate />
-      {posts.edges.map(({ node }) => (
+    <Home>
+      <div className="home">
+        {posts.edges.map(({ node }) => (
           <Post key={node.id} post={node} />
         ))}
-        <button onClick={loadMore}> Load More</button>
-    </div>
-    </>
+      </div>
+      <div className="content-btn">
+        <button onClick={loadMore} className="btn-load-posts"> Load More</button>
+      </div>
+    </Home>
   )
   
 }
