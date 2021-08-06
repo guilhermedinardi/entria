835a7fdde7af5548911577d904914af86adfa9a8
Guilherme Dinardi <gdinardito@gmail.com>
2021-08-06 17:44:18 -0300

    fix styled component on Modal.js initialized a css logic but i dont make working

--- client/crud/src/Components/modal/Modal.js
+++ client/crud/src/Components/modal/Modal.js
@@ -1,25 +1,29 @@
 import React from 'react'
 import styled from 'styled-components'
 
+ const ModalContainer = styled.div`
+    display: ${props => props.isOpen ? 'flex' : null}; 
+    width: 20em;
+    height: 10em;
+    background-color: #474973;
+`  
+ const Container = styled.div`
+`
+const ButtonCloseModal = styled.button``
 
-
-
-
-
-
-const Modal = ({id= 'modal' , onClose = () =>{}, children }) => {
-    const handleOutsideClick = (e) => {
-        if(e.target.id === id) onClose()
-    }
-    return (
-        
-            <div id={id} className="modal" onClick={handleOutsideClick}>
-                <div className="container">
-                    <button className="close-modal" onClick={onClose}>X</button>
-                    <div className="content">{children}</div>
-                </div>
-            </div>
-        
+const Content = styled.div``
+ 
+const Modal = (props) => {
+    const onClickClose = (e) => { 
+        props.onClose(e.target.value)
+    } 
+    return (    
+        <ModalContainer>
+            <Container>
+                <ButtonCloseModal onClick={onClickClose}>X</ButtonCloseModal>
+                <Content>{props.children}</Content>
+            </Container>
+        </ModalContainer>
     )
 }
 export default Modal
