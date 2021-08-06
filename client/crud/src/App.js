0cec58f0bbda83b1af5de4409a8f5635abda3782
Guilherme Dinardi <gdinardito@gmail.com>
2021-08-06 17:39:08 -0300

    fix styled component on App.js

--- client/crud/src/App.js
+++ client/crud/src/App.js
@@ -1,4 +1,3 @@
-
 import { React, useState } from 'react';
 import {  useLazyLoadQuery } from 'react-relay';
 import graphql from 'babel-plugin-relay/macro';
@@ -8,36 +7,38 @@ import PostCreate from './Components/create/PostCreate'
 import PostHome from './Components/PostHome'
 import styled from 'styled-components'
 
-const Container = styled.div`
+const Section = styled.div`
+  font-family: 'Work Sans', sans-serif;
   max-width: 1140px;
   margin: 0px auto;
   padding: 20px;
-  .post-create{
-      display: flex;
-      flex-wrap: wrap;
-      padding: 1em;
-      justify-content: space-between;
-      align-items: center;
-      .title{
-        width: 60%;
-        font-size: 1.5em;
-        letter-spacing: 0.5px;
-      }
-      .btn-add-post{
-        border: none;
-        border-radius: 15px;
-        width: 30%;
-        height: 6vh;
-        background-color: #5173E3;
-        color: #ffffff;
-        cursor: pointer;
-        font-size: 1.5em;
-        &:hover{
-          background: #ffffff;
-          color: #5173E3;
-        }
-      }
-    }
+`
+const Container = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  padding: 1em;
+  justify-content: space-between;
+  align-items: center;
+`
+const Title = styled.p`
+  width: 60%;
+  font-size: 1.5em;
+  letter-spacing: 0.5px;
+  color: #161B33;
+`
+const ButtonAddPost = styled.button`
+  border: none;
+  border-radius: 15px;
+  width: 10em;
+  height: 2.5em;
+  background-color: #A69CAC;
+  color: #ffffff;
+  cursor: pointer;
+  font-size: 0.9em;
+  &:hover{
+    background: #ffffff;
+    color: #161B33;
+  }
 `
 
 const App = () => {
@@ -49,27 +50,26 @@ const App = () => {
     `,
   )
 
-  const [show, setShow] = useState(false);
+   const [show, setShow] = useState(false); 
 
   return (
-    <Container>
-      <div className="post-create">
-        <p className="title"> VUTTR <br/> 
+    <Section>
+      <Container>
+        <Title> VUTTR <br/> 
           Very Userful Tools to Remember
-        </p>
-        <button onClick={() => setShow(!show)} type="button" className="btn-add-post"> Add + </button>
-        { 
-        show 
-        ? 
-        <Modal onClose={() => setShow(!show)}> 
-          <PostCreate /> 
-        </Modal>
-        : 
-        null 
+        </Title>
+        <ButtonAddPost onClick={() => setShow(!show)} type="button"> Add + </ButtonAddPost>
+        { show
+        ?
+          <Modal onClose={() => setShow(!show)}> 
+            <PostCreate /> 
+          </Modal> 
+        :
+        null
         }
-      </div>  
+      </Container>  
       <PostHome query={query}/>
-    </Container>
+    </Section>
   )
 }
 export default App;
